8d07220a5b923b78d0914888f8c373d1159deee6
skepee <skepee01@gmail.com>
2022-12-26 16:27:05 +0000

    tidy up, adding help.

---
+++ BenchmarkRest/Program.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using NLog;
+using NLog.Web;
+using System;
+using System.Threading.Tasks;
+
+namespace BenchmarkRestGet
+{
+    enum Method
+    { 
+        Get,
+        Post,
+        Put,
+        Patch,
+        Delete
+    }
+
+    class Program
+    {
+        private static Parameters myParams { get; set; }
+
+        static async Task Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("parameters: ");
+                Console.WriteLine(" - arg[0]: string, url to test");
+                Console.WriteLine(" - arg[1]: integer, number of iterations");
+                Console.WriteLine(" - arg[2]: http verb: Get (g), Post (po), Put (pu), Delete (del)(d)");
+                Console.WriteLine(" - arg[3]: integer, id starting value for Put and Delete. If missing id starting value is equal to 1");
+                Console.WriteLine(" - arg[4]: string, fromBody json for Put and Delete.");
+                return;
+            }
+
+            if (args.Length <= 1 || args.Length > 5)
+            {
+                Console.WriteLine("not valid input arguments. Unable to continue.");
+                return;
+            }
+
+            myParams = new Parameters(args);
+
+            var nlogger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+
+            using ILoggerFactory loggerFactory =
+                       LoggerFactory.Create(builder =>
+                           builder.AddSimpleConsole(options =>
+                           {
+                               options.IncludeScopes = true;
+                               options.SingleLine = true;
+                               options.TimestampFormat = "hh:mm:ss ";
+                           }).AddConsole()
+                             .AddDebug()
+                             );
+
+            ILogger<Program> logger = loggerFactory.CreateLogger<Program>();
+
+            logger.LogInformation("Application started...");
+
+            var builder = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHttpClient();
+                    services.AddTransient<MyHttpHandler>();
+
+                }).UseConsoleLifetime();
+
+            builder.ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.AddConsole();
+                //logging.SetMinimumLevel(LogLevel.Trace);
+            }).UseNLog();
+
+            var host = builder.Build();
+
+            using (var serviceScope = host.Services.CreateScope())
+            {
+                var services = serviceScope.ServiceProvider;
+
+                try
+                {
+                    var myService = services.GetRequiredService<MyHttpHandler>();
+                    await myService.Run(myParams);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error Occured: {ex.Message}");
+                }
+            }
+            NLog.LogManager.Shutdown();
+        }
+    }
+}
